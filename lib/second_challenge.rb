6591ec19e712f1a199fe5bc14bb3a1dbaaed50ff
Devon James Wright <devonjamess.wright@gmail.com>
2019-12-16 10:12:03 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -14,10 +14,14 @@ def second_challenge
      
     
    }
-      }, 
+      }
     :capulet => {
-      #your key/value pairs here!
-      }}
+      {:patriarch => {} },
+       {:matriarch => {} } ,
+       {:heroine => {} },
+       {:heroine_friends => []
+     } 
+      }
 
   
 end
