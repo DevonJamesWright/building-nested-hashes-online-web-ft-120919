a3a1ca8313fcc6bcd89a2c552a03904b4db11726
Devon James Wright <devonjamess.wright@gmail.com>
2019-12-16 10:28:56 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,37 +1,31 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
+      :patriarch =>
         {:name => "Lord Montague"},
-          {:age => '53'}
-        },
-      :matriarch => {
+          {:age => '53'},
+      :matriarch =>
         {:name => "Lady Montague"},
-          {:age => "54"}},
-      :hero => {
+          {:age => "54"},
+      :hero =>
         {:name => "Romeo"},
           {:age => "15"}
-            {:status => "alive"}
-        },
+            {:status => "alive"},
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
+      :patriarch =>
         {:name => "Lord Capulet"},
-          {:age => "50"}
-        },
-      :matriarch => {
+          {:age => "50"},
+      :matriarch =>
         {:name => "Lady Capulet"},
-          {:age => "51"}
-        },
-      :heroine => {
+          {:age => "51"},
+      :heroine =>
         {:name => "Juliet"},
           {:age => "15"}
-            {:status => "alive"}
-        },
+            {:status => "alive"},
       :heroine_friends => []
    }
-  }
 
   
 end
