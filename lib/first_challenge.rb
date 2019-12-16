2e47c4beddb3a358647505dcab8573b0e3cb15e8
Devon James Wright <devonjamess.wright@gmail.com>
2019-12-16 10:09:49 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,23 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     :montague => {
+      {:patriach => {}
+        
+      },
+      {:matriarch => {}
+        
+      },
+      {:hero => {}
+        
+      },
+   {:hero_friends => []
+     
+    
+   }
+     :capulet => {:patriarch => {} },
+       {:matriarch => {} } ,
+       {:heroine => {} },
+       {:heroine_friends => []
+     } 
+   }
 end
